82a26ba2bd788a562913e18e0a8452ffad33be47
rootkit7628 <arleme.dev7@gmail.com>
2022-01-23 22:55:40 +0300

    Init object verif

---
+++ verif.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+class Verif:
+    """
+        Objet de verification des erreurs et problèmes des
+        applications et outils dans le serveur
+    """
+    def __init__(self) -> None:
+        pass
