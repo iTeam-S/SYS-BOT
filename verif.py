b84b907b8b15613f2e34a270a55ef19143769b20
sergioDev22 <louis.sergio.rakotoarisoa@esti.mg>
2022-01-24 23:49:41 +0300

    Ajout de methode pour la verification de l'API

--- verif.py
+++ verif.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from paramiko import SSHClient, AutoAddPolicy
+import requests
 
 
 class Verif:
@@ -27,3 +28,89 @@ class Verif:
                 "status": False,
                 "message": str(err)
             }
+            
+    def verif_api(self,method,url,data=""):
+        
+        """
+            Methode pour verifier avec des 
+            tests les API disponible dans le serveur
+        """
+        
+        if method == "GET":
+            try: 
+                testing = requests.get(url)
+                if testing.status_code == 200:
+                    return {
+                        "error":False,
+                        "message":"OK"
+                    }
+                else:
+                    return {
+                        "error": True,
+                        "status_code":testing.status_code,
+                        "raison":str(testing.reason),
+                        "message":f"Probléme sur l'API <{url}> du methode GET"
+                    }
+                    
+            except Exception as err:
+                return {
+                    "error":True,
+                    "url":url,
+                    "message":str(err)
+                }
+                
+        elif method == "POST":
+            try:
+                header = {'content-type': 'application/json; charset=utf-8'}
+                testing = requests.post(url,json=data,headers=header)
+                
+                if testing:
+                    if testing.status_code==200:
+                        return {
+                            "error":False,
+                            "message":"OK"
+                        }
+                    else:
+                        return {
+                            "error": True,
+                            "status_code":testing.status_code,
+                            "raison":str(testing.reason),
+                            "message":f"Probléme sur l'API <{url}> du methode POST"
+                        }
+                else:
+                    return {
+                        "error":True,
+                        "message":f"L'API <{url}> du methode POST renvoie RIEN"
+                    }
+                    
+            except Exception as err:
+                return {
+                    "error":True,
+                    "url":url,
+                    "message":str(err)
+                }
+                
+        elif method == "PUT":
+            try:
+                header = {'content-type': 'application/json; charset=utf-8'}
+                testing = requests.put(url,data=data,headers=header)
+                
+                if testing.status_code == 200:
+                    return {
+                        "error":False,
+                        "message":"OK"
+                    }
+                else:
+                        return {
+                            "error": True,
+                            "status_code":testing.status_code,
+                            "raison":str(testing.reason),
+                            "message":f"Probléme sur l'API <{url}> du methode PUT"
+                        } 
+                        
+            except Exception as err:
+                return {
+                    "error":True,
+                    "message":str(err)
+                }
+            
