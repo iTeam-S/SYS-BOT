0f93c8d3ff8b78224ebb3f1d659ffbafdade6e0b
rootkit7628 <arleme.dev7@gmail.com>
2022-01-24 16:18:10 +0300

    [ADD] Add ssh verification

--- verif.py
+++ verif.py
@@ -1,9 +1,29 @@
 #!/usr/bin/python
+from paramiko import SSHClient, AutoAddPolicy
+
 
 class Verif:
-    """
-        Objet de verification des erreurs et problèmes des
+    '''
+        Objet de verification des erreurs et problemes des
         applications et outils dans le serveur
-    """
+    '''
     def __init__(self) -> None:
         pass
+
+    def ssh(self, host, username, password, port=22) -> dict:
+        try:
+            client = SSHClient()
+            client.set_missing_host_key_policy(AutoAddPolicy())
+            client.connect(host, port, username, password, timeout=10)
+            client.close()
+
+            return {
+                "status": True,
+                "message": "connection success"
+            }
+
+        except Exception as err:
+            return {
+                "status": False,
+                "message": str(err)
+            }
